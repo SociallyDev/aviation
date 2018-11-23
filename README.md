9dfb626e04ccd9f964a7f0c31acc7dfe99de403d
Devang Srivastava <SociallyDev@users.noreply.github.com>
2018-11-24 05:26:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,180 @@
+# Aviation
+### URL routing inspired by express for the frontend!
+
+Aviation helps you build simple javascript apps that **run right in the browser**.
+No server side processing needed.
+
+<br />
+
+Aviation **automatically tracks link clicks, history events and page loads**, and invokes your functions.
+
+<br />
+
++ Create dynamic websites (especially dashboards) easily.
++ In-built response functions for stuff like changing content and setting cookies.
++ Parses URLs, queries, cookies and parameters for you.
+
+<br />
+
+# Getting Started
+
+First include Aviation in your code
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/aviation"></script>
+```
+
+Let's create a simple app that shows a welcome message on page load!
+
+```javascript
+
+var app = Aviation()
+
+app.use(function(req, res, next) {
+  res.html("<h1>Welcome Home</h1>")
+})
+```
+
+<br />
+Now, let's unleash Aviation's capabilities with a dynamic example.
+
+In this example, we'll create a menu on page load.
+
+
+We'll be able to **browse to different pages, without actually reloading the page**.
+
+```javascript
+
+var app = Aviation()
+
+app.on("/", function(req, res, next) {
+  res.page("Home").html("<h1>Welcome Home</h1> <a href='/news'>View News</a>")
+})
+
+app.on("/news", function(req, res, next) {
+  res.page("News").html("<h1>News</h1> <h3>Aviation takes flight</h3> <a href='/'>Back Home</a>'")
+})
+```
+
+Here's the result:
+
+![gif](https://media.giphy.com/media/28LM3yKnqjuY6I7zbn/giphy.gif)
+<br />
+
+
+# Functions
+
+## Aviation(options)
+
+This creates a new instance of Aviation with the supplied options.
+Possible options are:
++ contentWrapper (String) This is the element res.html() will update with content. Defaults to `body`.
++ onError (Function) This is the function that will be called when an error occurs. Defauls to `console.error`.
++ caseSensitive - (Boolean) Should /app and /App not be considered the same? Defaults to false.
++ strict - (Boolean) Should /app and /app/ not be considered the same? Defaults to false.
++ removeFromPath - (String or Regex) Removes the string or matching regex from all paths. Useful for running local projects, or anything inside a directory.
++ event - (String) The event type to listen for. Defaults to `click`.
++ source - (String) The source types to listen for. Defaults to `a:not([target='_blank'])`.
+
+
+ Returns the `.use() `and `.handle()` functions.
+
+
+<br />
+
+## .use(callback) or .use(path, callback)
+
+This function registers a callback that Aviation will call when a request that matches its path is received. It also has an alias, `.on(path, callback)`.
+
+**The path can be an array of paths, or a simple string, or a regex.**
+When a path isn't supplied, all requests are routed to that callback.
+
+
+### Advanced Paths
+
+You can specify parameters in the path. Aviation will expose these using `req.params`.
+
+```javascript
+app.use("/users/:user/orders/:order", function(req) { console.log(req.params) })
+```
+
+
+You can also specify wildcards.
+For example, Aviation will send a request for /users/dev/orders to the following function.
+
+```javascript
+app.use("/users/*", function(req) { console.log(req.url) })
+```
+
+
+<br />
+
+## .handle(url)
+
+This function handles loading a new URL.
+It can also handle events. Advanced usages can directly call this when they want to use Aviation rather than waiting for someone to click a link.
+
+
+<br />
+
+## Callback Functions
+
+Whenever Aviation invokes a callback, it provides three functions as arguments.
+
+```javascript
+function myCallback(request, response, next) {
+}
+```
+
+### Request
++ url - This is the URL of the request.
++ hostname - This is the hostname (domain) of the request.
++ pathname - This is the path of the request, like /app.
++ protocol - This is the protocol of the request.
++ secure - Boolean, whether the request is over HTTPS.
++ query - Key-value object, this is the parsed query string of the URL.
++ params - Key-value object, these are the parameters for URL requests.
++ cookies - Key-value object, these are the parsed browser cookies.
++ aviation - A reference to the Aviation instance.
+
+
+### Response
++ **cookie(key, value, options)** - Saves a cookie in the browser. Options can have: `maxAge`, `expires`, `path`, `domain`, `secure` or `sameSite`.
+
+<br />
+
++ **clearCookie(key, options)** - Deletes a cookie from the browser. Options other than `maxAge` and `expires` must be the same or cookie deletion won't work.
+
+<br />
+
++ **redirect(url)** - Redirects to this URL. If Aviation has a callback for this URL, it'll be called, else a real redirection will occur. It also has an alias, `location(url)`.
+
+<br />
+
++ **page(title, url)** - Updates the page title and creates a history event. If no URL is specified, the request URL will be used.
+
+<br />
+
++ **html(content)** - Essentially a wrapper for jQuery's .html(). Updates the content of the page.
+
+
+<br />
+
+
+### Next()
+
+Invoking `next()` makes Aviation run the next matching function for this request.
+
+
+```javascript
+
+app.use(function(req, res, next) {
+  //Some error occured, skip to next function.
+  next()
+})
+
+app.use(function(req, res, next) {
+  console.log("Running backup function")
+})
+
+```
---
+++ aviation.js
@@ -0,0 +1,183 @@
+/*
+URL routing for frontend apps!
++ Inspired by express.
+
+
+Written by Dev Ang ( https://github.com/SociallyDev ) on 23rd November 2018.
+
+
+MIT License ( https://opensource.org/licenses/MIT )
+
+*/
+
+
+if(typeof $ == "undefined") { throw new Error("Aviation requires jQuery for now.") }
+
+
+
+function Aviation(options) {
+  if(!(this instanceof Aviation)) { return new Aviation(options) }
+  if(typeof options !== "object") { options = {strict: false, caseSensitive: false} }
+  var callbacks = [], aviation = this, contentWrapper = $(options.contentWrapper || "body"), onError = options.onError || console.error
+
+
+
+  /*
+  Adds callbacks.
+  */
+  this.use = function(match, callback) {
+    var path = match, fn = callback, keys = []
+    if(!callback) { fn = match, path = /^.*$/ }
+
+    //Convert path to regex.
+    if(!(path instanceof RegExp)) {
+      if(path.constructor == Array || path.constructor == Object) { for(var i in path) { aviation.use(path[i], callback) }; return aviation }
+      else { path = String(path) }
+
+      path = $("<a href='" + path + "'></a>").get(0).pathname
+      if(!options.strict) { path = path.replace(/\/$/, "") }
+      if(!options.caseSensitive) { path = path.toLowerCase() }
+      if(options.removeFromPath) { path = path.replace((options.caseSensitive || typeof options.removeFromPath !== "string"  ? options.removeFromPath : options.removeFromPath.toLowerCase()), "") }
+
+      //Handle params.
+      if(/\:/.test(path)) {
+        path = path.replace(/\*+/g, "").replace(/:([^\/]+)/g, function(a, key) { keys.push(key); return "([^\\/]+?)" })
+        path = new RegExp("^" + path.split("([^\\/]+?)").map(function(p) { return p.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&") }).join("([^\\/]+?)") + "$")
+      }
+      else {
+        path = new RegExp("^" + path.split(/\*+/).map(function(p) { return p.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&") }).join(".*") + "$")
+      }
+    }
+
+    callbacks.push({
+      path: path,
+      keys: keys,
+      fn: fn
+    })
+    return aviation
+  }
+  this.on = this.use
+
+
+
+  /*
+  Handles requests.
+  */
+  this.handle = function(url) {
+    var exactURL = (typeof url == "string" ? url : (typeof url !== "object" ? "/" : (url.href ? url.href : (this.href ? this.href : (this.action ? this.action : "/")))))
+    var el = $("<a href=" + exactURL + "></a>").get(0), path = el.pathname
+
+    if(!options.strict) { path = path.replace(/\/$/, "") }
+    if(!options.caseSensitive) { path = path.toLowerCase() }
+    if(options.removeFromPath) { path = path.replace((options.caseSensitive ? options.removeFromPath : options.removeFromPath.toLowerCase()), "") }
+
+    //Create a list of viable callbacks.
+    var viableCbs = []
+    for(var i in callbacks) {
+      var cb = callbacks[i]
+      if(cb.path.test(path)) {
+        var params = {}, vals = path.match(cb.path).slice(1)
+        if(cb.keys.length) { for(var i in vals) { params[cb.keys[i]] = vals[i] } }
+        viableCbs.push({fn: cb.fn, params: params})
+      }
+    }
+    if(!viableCbs.length) { return aviation }
+
+    //Create request, response and next functions.
+    if(url.preventDefault) { url.preventDefault() }
+    var request = {
+      url: el.href,
+      hostname: el.hostname,
+      pathname: path,
+      protocol: (el.protocol ? el.protocol.replace(":", "") : null),
+      secure: false,
+      query: parseQuery(el.href),
+      params: {},
+      cookies: parseCookies(),
+      aviation: aviation
+    }
+    if(request.protocol == "https") { request.secure = true }
+    var response = {
+      cookie: function(key, val, options) {
+        if(!options) { options = {} }
+        request.cookies[key] = val
+        if(typeof options.maxAge == "undefined" && typeof options.expires == "undefined") { options.maxAge = 2.628e+6 }
+        document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(val)
+        + (options.maxAge ? ";max-age=" + encodeURIComponent(options.maxAge) : "")
+        + (options.expires ? ";expires=" + encodeURIComponent(options.expires) : "")
+        + (options.path ? ";path=" + encodeURIComponent(options.path) : ";path")
+        + (options.domain ? ";domain=" + encodeURIComponent(options.domain) : "")
+        + (options.secure ? ";secure=" + encodeURIComponent(options.secure) : "")
+        + (options.sameSite ? ";samesite=" + encodeURIComponent(options.sameSite) : "")
+        return this
+      },
+      clearCookie: function(key, options) {
+        options.maxAge = -1
+        this.cookie(key, null, options)
+        delete request.cookies[key]
+        return this
+      },
+      location: function(url) {
+        var redirect = true
+        aviation.handle({href: url, preventDefault: function() { redirect = false }})
+        if(redirect) { window.location = url }
+        return this
+      },
+      redirect: function(url) { return this.location(url) },
+      page: function(title, url) {
+        if(typeof history !== "undefined" && history.pushState) { history.pushState({}, title, url || request.url) }
+        $("title").text(title)
+        return this
+      },
+      html: function(content) {
+        contentWrapper.html(content)
+      }
+    }
+    var next = function(e) {
+      try {
+        if(e) { throw e }
+        i++
+        var cb = viableCbs[i]
+        if(!cb) { return }
+        request.params = cb.params
+        cb.fn(request, response, next)
+      }
+      catch(e) { onError(e) }
+    }, i = -1
+
+    //Start request.
+    next()
+
+    return aviation
+  }
+
+
+  //Setup calls.
+  $(document).ready(function() { aviation.handle(window.location) })
+  $(document).on(options.event || "click", options.source || "a:not([target='_blank'])", aviation.handle)
+  window.addEventListener("popstate", function(e) { aviation.handle(window.location.href) }, false)
+
+
+
+  /*
+  Parses query.
+  */
+  var parseQuery = function(qs) {
+    if(!/\?/.test(qs)) { return {} }
+    var query = {}, pairs = qs.split("?")[1].split("&")
+    for(var i in pairs) { var pair = pairs[i].split("="); query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]) }
+    return query
+  }
+
+
+
+  /*
+  Parses cookies.
+  */
+  var parseCookies = function() {
+    var cookies = {}, cookie = document.cookie.split(";")
+    for(var i in cookie) { var pair = cookie[i].split("="); if(!pair[0] || !pair[1]) { continue }; cookies[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]) }
+    return cookies
+  }
+
+}
